f282f6ecac120185482c60dc0d3102732ecad69b
artegrr <foster2196@gmail.com>
2020-05-12 22:46:03 +0300

    Update main.js
    add new code

--- app/main.js
+++ app/main.js
@@ -1,18 +1,20 @@
-let name = prompt("Твое имя, падаван?", "");
-alert(`Приветствую ${name}!`);
+// let name = prompt("Твое имя, падаван?", "");
+// alert(`Приветствую ${name}!`);
 
-age = prompt("Давай узнаем твой возраст по году рождения?");
-const today = new Date(2020);
-alert(`Тебе ${today - age}!`);
+// age = prompt("Давай узнаем твой возраст по году рождения?");
+// const today = new Date(2020);
+// alert(`Тебе ${today - age}!`);
 
-let length = prompt(
-  "Введите длину стороны вашего квадрата, а я посчитаю его периметр."
-);
-length = 4 * length;
-alert(length);
+// let length = prompt(
+//   "Введите длину стороны вашего квадрата, а я посчитаю его периметр."
+// );
+// length = 4 * length;
+// alert(length);
 
-radius = prompt ("А теперь посчитаем площадь круга.")
-alert (2* Math.PI * radius);
+// radius = prompt ("А теперь посчитаем площадь круга.")
+// alert (2* Math.PI * radius);
 
-let convert = confirm("Может сконвертируем валюту?");
-
+let dollarRate = prompt("Может сконвертируем валюту?");
+const euroRate = 0.92;
+let count = dollarRate * euroRate;
+alert(`Получаем ${count} €`);
