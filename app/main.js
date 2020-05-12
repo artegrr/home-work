23be74c1aa3b587394eb89a60d1e22d03a55f407
artegrr <foster2196@gmail.com>
2020-05-12 19:47:25 +0300

    docs: add my files

---
+++ app/main.js
@@ -0,0 +1,19 @@
+// let name = prompt("Твое имя, падаван?", "");
+// alert(`Приветствую ${name}!`);
+
+// age = prompt("Давай узнаем твой возраст по году рождения?");
+// const today = new Date(2020);
+// alert(`Тебе ${today - age}!`);
+
+// let length = prompt(
+//   "Введите длину стороны вашего квадрата, а я посчитаю его периметр."
+// );
+// length = 4 * length;
+// alert(length);
+
+// radius = prompt ("А теперь посчитаем площадь круга.")
+// alert (2* Math.PI * radius);
+
+let convert = confirm("Может сконвертируем валюту?");
+const currency = 
+alert()
